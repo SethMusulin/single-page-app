e6ae848c46b1d013125413894aa76ee7a41f0cf0
Bebe Peng <bebepeng@gmail.com>
2014-06-11 15:42:59 -0600

    beefed up test for people index
    Signed-off-by: Seth Musulin <sethmusulin@gmail.com>

--- spec/features/contact_manager_spec.rb
+++ spec/features/contact_manager_spec.rb
@@ -8,12 +8,17 @@ feature 'The one-page contact manager app' do
   end
 
   scenario 'People are printed on the home page', js: true do
-    Person.create(first_name: 'Joe', last_name: 'Example', address: 'somewhere, Lala Land')
-    Person.create(first_name: 'Edna', last_name: 'Example', address: 'somewhere, Lala Land')
+    Person.create(first_name: 'Joe',
+                  last_name: 'Example',
+                  address: 'somewhere, Lala Land')
+    Person.create(first_name: 'Edna',
+                  last_name: 'Example',
+                  address: 'somewhere, Lala Land')
 
     visit '/'
-    expect(page).to have_content("Joe")
-    expect(page).to have_content("Edna")
+    expect(page).to have_content("Joe Example")
+    expect(page).to have_content("somewhere, Lala Land")
+    expect(page).to have_content("Edna Example")
   end
 
 end
