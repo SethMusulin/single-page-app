b01eff9c4a33050a40385855dc854203c2111232
Bebe Peng <bebepeng@gmail.com>
2014-06-11 17:31:31 -0600

    [#73066244] People can be added to the DOM
    Signed-off-by: Seth Musulin <sethmusulin@gmail.com>

--- app/assets/javascripts/people_app.js
+++ app/assets/javascripts/people_app.js
@@ -1,11 +1,36 @@
 window.PeopleApp = {
+
   initialize: function (path) {
     $.getJSON(path, this.printAllPeople);
+    $(document).on("submit", "[data-behavior=create-person]", this.addPersonForm.bind(this));
   },
 
   printAllPeople: function (response) {
+    $("[data-container=main]").append(JST['layouts/new']({url: response._links.self.href}));
+
+
     $.each(response._embedded.people, function(){
       $("[data-container=people]").append(JST['layouts/index'](this));
     });
+  },
+
+  addPersonForm: function (event) {
+    event.preventDefault();
+
+    var person_data = {};
+    $.each(event.target, function(){
+      person_data[this.name] = this.value;
+    });
+    var form = event.target
+    form.reset();
+    $.post(event.target.action, JSON.stringify(person_data), this.successfulPostOrNot,'json');
+  },
+
+  successfulPostOrNot: function (response) {
+    $("[data-container=people]").append(JST['layouts/index'](response));
+
+    //if response is the stuff we inputed the it's successful
+    //then we append
+    //if it's an error or something it's not
   }
 };
